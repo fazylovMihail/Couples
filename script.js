55bb07a9e9bff9f4d18c5fbe7ebf02ef6363cd14
Fazylov Mihail <161601509+fazylovMihail@users.noreply.github.com>
2024-11-26 17:38:19 +0500

    Работа над созданием кнопок - игрового контента

--- script.js
+++ script.js
@@ -9,8 +9,27 @@ const submitBtn = document.querySelector(submitBtnSelector);
 const inputContent = document.querySelector(inputContentSelector);
 
 // основной код
-function createBoard(){
+function createBoard(columns, count){
     board.innerHTML = null;
+
+    const template = document.querySelector('#gameTableTemplate').cloneNode(true).content;
+    const table = template.querySelector('.table');
+    const tableBtn = template.querySelector('.table-btn');
+
+    table.style.display = 'grid';
+    table.style = `
+        grid-template-columns: repeat(${columns}, 1fr);
+        grid-template-rows: repeat(${columns}, 1fr);
+    `;
+
+    board.append(table);
+    board.append(tableBtn);
+
+    tableBtn.addEventListener('click',()=>{location.reload()})
+
+    for(let i = 0;i<count;i++){
+        table.append(CreateCard());
+    }
 }
 
 submitBtn.addEventListener('click',(event)=>{
@@ -18,15 +37,22 @@ submitBtn.addEventListener('click',(event)=>{
 
     const inputContentValue = inputContent.value;
 
-    if(inputContentValue >= 2 && inputContentValue <= 6){
+    if(inputContentValue >= 2 && inputContentValue <= 6 && inputContentValue%2==0){
         const totalValue = inputContentValue * inputContentValue;
 
         console.log(inputContentValue);
         console.log(totalValue);
 
-        createBoard();
+        createBoard(inputContentValue, totalValue);
     }
     else{
-        alert('Пожалуйста запиши чётное число в указанном диапазоне')
+        alert('Пожалуйста запиши чётное число в указанном диапазоне');
     }
 })
+
+function CreateCard(){
+    const cardTemplate = document.querySelector('#card-template').cloneNode(true).content;
+    const card = cardTemplate.querySelector('.card');
+
+    return card;
+}
